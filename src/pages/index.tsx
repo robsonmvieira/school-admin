81200a55b12e7208c8f60767b82293b03f04f3e5
Robson Maia <robsonmvieira@gmail.com>
2022-02-11 22:27:53 -0300

    ✨app page

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  
+  return (
+    <h1 className="text-red-400">Login</h1>
+  )
+};
+
+export default Home;
+
+// https://tailwindcollections-pos-dark-dashboard.netlify.app/
+
+// https://jobick.dexignlab.com/xhtml/job-list.html
