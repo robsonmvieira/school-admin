97a96a4008d512181a4446962e5c69a2c65bdfae
Robson Maia <robsonmvieira@gmail.com>
2022-02-14 19:35:56 -0300

    🚨applied linter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,12 @@
-import type { NextPage } from "next";
+import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  
   return (
     <h1 className="text-red-400">Login</h1>
   )
-};
+}
 
-export default Home;
+export default Home
 
 // https://tailwindcollections-pos-dark-dashboard.netlify.app/
 
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,37 +1,36 @@
-import TextField from "../../components/shared/forms/text-field";
+import TextField from '../../components/shared/forms/text-field'
 
-import { useForm } from "react-hook-form";
+import { useForm } from 'react-hook-form'
 
-import { AiOutlineMail } from "react-icons/ai";
-import { RiLockPasswordFill } from "react-icons/ri";
+import { AiOutlineMail } from 'react-icons/ai'
+import { RiLockPasswordLine } from 'react-icons/ri'
 
-
-export default function Login() {
-  const onSubmitHandler = (data: any) => console.log(data);
-  const { register, handleSubmit } = useForm();
-  const url = `https://images.unsplash.com/photo-1583468982228-19f19164aee2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1011&q=80`;
+export default function Login () {
+  const onSubmitHandler = (data: any) => console.log(data)
+  const { register, handleSubmit } = useForm()
+  const url = 'https://images.unsplash.com/photo-1583468982228-19f19164aee2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1011&q=80'
   return (
     <div className="flex h-screen">
       <div
         style={{ backgroundImage: `url(${url})` }}
         className="w-1/2 bg-cover bg-no-repeat  bg-center"
       ></div>
-      <div className="w-1/2 flex justify-center items-center bg-gray-100">
+      <div className='w-1/2 flex justify-center items-center bg-gray-100'>
         <div className="w-5/12 flex flex-col gap-x-8">
           <h1 className="text-5xl font-sans text-gray-900">Faça Login</h1>
           <form onSubmit={handleSubmit(onSubmitHandler)}>
               <TextField
-                inputClasses="h-10 w-full bg-gray-200 ml-4 outline-none"
                 containerClasses='mt-14 flex items-center bg-gray-200  rounded-lg px-4'
+                inputClasses="h-11 w-full bg-gray-200 ml-4 outline-none"
                 icon={<AiOutlineMail size={24} />}
                 type="email"
                 {...register('email')}
                 id="email"
               />
               <TextField
-                inputClasses="h-10 w-full bg-gray-200 ml-4 outline-none"
                 containerClasses='mt-10 flex items-center bg-gray-200  rounded-lg px-4'
-                icon={<RiLockPasswordFill />}
+                inputClasses="h-11 w-full bg-gray-200 ml-4 outline-none"
+                icon={<RiLockPasswordLine size={28} />}
                 type="password"
                 {...register('password')}
                 id="password"
@@ -52,5 +51,5 @@ export default function Login() {
         </div>
       </div>
     </div>
-  );
+  )
 }
