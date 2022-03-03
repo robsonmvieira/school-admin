a5c228bf9c3ed6aeef806a05c5f5f40592e8ce48
Robson Maia <robsonmvieira@gmail.com>
2022-03-02 21:40:24 -0300

    ♻️ refactor sidebar

--- src/components/sidebarItem.tsx
+++ src/components/sidebarItem.tsx
@@ -1,14 +1,19 @@
-import { ReactNode } from 'react';
+import { forwardRef, ReactNode } from 'react';
 export type SidebarMenuItemProps = {
   title: string;
   icon: ReactNode;
-  isSideBarOpened: boolean
+  isSideBarOpened: boolean,
+
 }
-export default function SidebarMenuItem({icon, title, isSideBarOpened }: SidebarMenuItemProps) {
-  return (
-    <div className="flex cursor-pointer">
-    { icon }
-    {isSideBarOpened && <span className="text-orange-300 font-sans text-lg" >{ title }</span>}
-  </div>
-  )
+
+const SidebarMenuItem = ({icon, title, isSideBarOpened, ...rest }: SidebarMenuItemProps, ref: any) => {
+
+      return (
+        <a className="flex cursor-pointer"  ref={ref} {...rest}>
+        { icon }
+        {isSideBarOpened && <span className="text-orange-300 font-sans text-lg" >{ title }</span>}
+      </a>
+      )
 }
+
+export default forwardRef(SidebarMenuItem)
