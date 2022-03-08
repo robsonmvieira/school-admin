73294065e16ad503e7c7bf13f331d345f5f444dd
Robson Maia <robsonmvieira@gmail.com>
2022-03-07 23:03:14 -0300

    ♻️ add category lista card with mocked data

--- src/pages/admin/categories/categoryCardList.tsx
+++ src/pages/admin/categories/categoryCardList.tsx
@@ -2,39 +2,41 @@ import { BsArrowUp, BsArrowDown } from "react-icons/bs"
 import { FiRefreshCw } from "react-icons/fi"
 import { HiOutlineCurrencyDollar } from "react-icons/hi"
 import CardReport from "../../../components/cardReport"
+import { useAdmin } from "../../../contexts/useAdmin"
 
 const CategoryCardList = () => {
+  const { categories } = useAdmin()
 
   return (
     <div className="flex gap-x-3">
     <CardReport
-      title="R$ 2.750,00"
-      headerTitle="+0.3%"
-      subtitle="Último vencimento"
+      title="Almoxarifado"
+      headerTitle="1"
+      subtitle="Crítica"
       headerArrowIcon={<BsArrowUp size={14} className='text-green-400' />}
       headerIcon={<HiOutlineCurrencyDollar size={28} className='text-violet-400' />}
     />
 
       <CardReport
-      title="R$ 450,00"
-      headerTitle="+1.5%"
-      subtitle="Vale Alimentação"
+      title="Cantina"
+      headerTitle="40"
+      subtitle="Mais itens cadastrados"
       headerArrowIcon={<BsArrowUp size={14} className='text-green-400' />}
       headerIcon={<HiOutlineCurrencyDollar size={28} className='text-violet-400' />}
     />
 
       <CardReport
-      title="R$ 0.00"
-      headerTitle="- 0%"
-      subtitle="Descontos"
+      title="Categorias"
+      headerTitle="0"
+      subtitle="Bloqueadas"
       headerArrowIcon={<BsArrowDown size={16} className='text-red-400 font-semibold' />}
       headerIcon={<FiRefreshCw size={22} className='text-violet-400' />}
     />
 
     <CardReport
-      title="Auxiliar Administrativo"
-      headerTitle="2"
-      subtitle="Promoções"
+      title={`Categorias`}
+      headerTitle={`${categories.length}`}
+      subtitle="Registradas"
       headerArrowIcon={<BsArrowUp size={14} className='text-green-400' />}
       headerIcon={ <HiOutlineCurrencyDollar size={28} className='text-violet-400' />}
     />
