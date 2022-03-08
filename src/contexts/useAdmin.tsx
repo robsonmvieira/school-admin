0ec8a4e1f083a2eae95b95fcd4e64f41315a24cf
Robson Maia <robsonmvieira@gmail.com>
2022-03-07 23:00:46 -0300

    ✨add new business logic in admin context

--- src/contexts/useAdmin.tsx
+++ src/contexts/useAdmin.tsx
@@ -1,5 +1,11 @@
-import {createContext, ReactNode, useContext, useEffect, useState} from 'react';
-import api from '../service/api.service';
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState
+} from 'react'
+import api from '../service/api.service'
 
 export class Category {
   id: string
@@ -13,25 +19,55 @@ export type AdminProviderProps = {
   children: ReactNode
 }
 
-
-export type AdminContextProps =  {
+export type AdminContextProps = {
   categories: Category[]
   addCategory: (categoryInput: Category) => void
+  editCategory: (categoryId: string) => void
+  deleteCategory: (categoryId: string) => void
+  moreDetailCategory: (categoryId: string) => void
+  selectedCategory: Category | null
 }
 
 const AdminContext = createContext<AdminContextProps>({} as AdminContextProps)
 
-
-export function AdminProvider({children}:AdminProviderProps) {
+export function AdminProvider({ children }: AdminProviderProps) {
   const [categories, setCategories] = useState<Category[]>([])
+  const [selectedCategory, setSelectedCategory] = useState<Category | null>(null)
 
   function addCategory(categoryInput: Category): void {
     setCategories(initialState => [...initialState, categoryInput])
   }
 
-  return <AdminContext.Provider value={{categories, addCategory}}>
-    {children}
+  const editCategory = (id: string) => {
+    console.log('edit', id)
+    onHandlerSelectedCategory(id)
+  }
+  const deleteCategory = (id: string) => {
+    console.log('remove', id)
+  }
+  const moreDetailCategory = (id: string) => {
+    console.log('more detail', id)
+  }
+
+  const onHandlerSelectedCategory = (categoryId: string) => {
+    const category = categories.find(category => category.id === categoryId)
+    setSelectedCategory(category ?? null);
+  }
+
+  return (
+    <AdminContext.Provider
+      value={{
+        selectedCategory,
+        categories,
+        addCategory,
+        editCategory,
+        deleteCategory,
+        moreDetailCategory
+      }}
+    >
+      {children}
     </AdminContext.Provider>
+  )
 }
 
 export function useAdmin() {
